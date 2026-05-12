b1195621bed1cbdbd0f50681ed97151b936f8ea3
amin najjar <aminnajjar3@gmail.com>
2026-05-12 09:30:21 +0100

    Version 1: Arborescence et POMs restaurés

---
+++ produits-service/target/classes/data.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO categorie (nom) VALUES ('Electronique'), ('Maison'), ('Mode');
+INSERT INTO produit (nom, prix, stock, categorie_id) VALUES ('Smartphone', 800.0, 10, 1), ('PC Portable', 1500.0, 5, 1);
